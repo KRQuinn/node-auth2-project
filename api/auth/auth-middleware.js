da53ea3663a6edf9a51fe7f7d2b0ac69070ac9e7
KRQuinn <kr.quinn82@gmail.com>
2021-11-09 15:52:42 -0800

    all tests passing

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken')
+const User = require('../users/users-model')
 
 const restricted = (req, res, next) => {
   /*
@@ -16,6 +18,23 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+    const token = req.headers.authorization
+    if (!token)
+      return next({
+        status: 401,
+        message: 'Token required',
+      })
+  
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err)
+        return next({
+          status: 401,
+          message: 'Token invalid',
+          realErrorMessage: err.message,
+        })
+      req.decodedJwt = decoded
+      next()
+    })
 }
 
 const only = role_name => (req, res, next) => {
@@ -29,10 +48,19 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
+    const { decodedJwt } = req
+    if (decodedJwt.role_name === role_name) {
+      next()
+    } else {
+      next({
+        status: 403,
+        message: 'This is not for you',
+      })
+    }
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -40,6 +68,17 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
+    try {
+      const [user] = await User.findBy({ username: req.body.username })
+      if (!user) {
+        next({ status: 401, message: 'Invalid credentials' })
+      } else {
+        req.user = user
+        next()
+      }
+    } catch (err) {
+      next(err)
+    }
 }
 
 
@@ -62,6 +101,25 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
+    let { role_name } = req.body
+
+    if (role_name) {
+      role_name = req.body.role_name.trim()
+    }
+    if (!role_name) {
+      req.role_name = 'student'
+      next()
+    } else if (role_name.trim() === 'admin') {
+      next({ status: 422, message: 'Role name can not be admin' })
+    } else if (role_name.trim().length > 32) {
+      next({
+        status: 422,
+        message: 'Role name can not be longer than 32 chars',
+      })
+    } else {
+      req.role_name = role_name
+      next()
+    }
 }
 
 module.exports = {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+
+const bcrypt = require('bcryptjs')
+
+const Users = require('../users/users-model')
+const tokenBuilder = require('./token-builder')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,6 +18,16 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+    const { username, password } = req.body
+    const { role_name } = req
+    const rounds = process.env.BCRYPT_ROUNDS || 8
+    const hash = bcrypt.hashSync(password, rounds)
+  
+    Users.add({ username, password: hash, role_name })
+      .then((newUser) => {
+        res.status(201).json(newUser)
+      })
+      .catch(next)
 });
 
 
@@ -37,6 +51,19 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+    let { username, password } = req.body
+
+    Users.findBy({ username }).then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenBuilder(user)
+        res.status(200).json({
+          message: `${user.username} is back!`,
+          token,
+        })
+      } else {
+        next({ status: 401, message: 'Invalid credentials' })
+      }
+    })
 });
 
 module.exports = router;
